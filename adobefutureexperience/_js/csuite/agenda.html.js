dba35fffbb5e82af2a3a83b47dd1c750dc1248e2
ntieszen <ntieszen+github@gmail.com>
2019-09-10 21:45:28 -0500

    09/10/2019: csuite page

---
+++ adobefutureexperience/_js/csuite/agenda.html.js
@@ -0,0 +1,39 @@
+//CSUITE Agenda
+document.write(
+	'<div class="agenda-separator">' +
+	'	<div class="redhr"></div>' +
+	'</div>' +
+        '<h1 class="brand-heading bebas-light capitalize">Adobe and Publicis.Sapient VIP Luncheon</h1>' +
+	'<h4 class="brand-caption bebas capitalize">' +
+	'       WE INVITE YOU TO THE FUTURE OF FINANCIAL SERVICES' +
+	'</h4>' +
+	'<h1 class="agenda-date bebas-bold">10.22.2018</h1>' + 
+	'<address class="agenda-address text-center capitalize" style="display:inline-block;">' +
+	'	Money2020 / Sands Expo 201 & 202 Level 1 / Las Vegas, NV' +
+	'</address>' +
+	'<div class="agenda-separator">' +
+	'	<div class="redhr"></div>' +
+	'</div>' +
+	'<div class="agenda-details text-left">' +
+	'	<table>' +
+	'		<tr>' +
+	'			<td>12:00pm</td>' +
+	'			<td>-</td>' +
+	'			<td>Check in and mingling</td>' +
+	'		</tr>							' +
+	'		<tr>' +
+	'			<td>12:15pm</td>' +
+	'			<td>-</td>' +
+	'			<td>' +
+	'				Lunch + topic discussion by Adobe and Publicis.Sapient leaders' +
+	'				<br /><small><em>[Take the robot out of the banker: How A.I. will help Gen Z ❤️ banks]</em></small>' +
+	'		        </td>' +
+	'		</tr>' +
+	//'		<tr>' +
+	//'			<td>1:00pm</td>' +
+	//'			<td>-</td>' +
+	//'			<td>Closing Remarks</td>' +
+	//'		</tr>' +
+	'	</table>' +
+	'</div>'
+);
---
+++ adobefutureexperience/_js/csuite/tagline.html.js
@@ -0,0 +1,12 @@
+//CSUITE Tagline
+document.write(
+    '<section class="tagline">' +
+    '    <div class="container-fluid">' +
+    '        <div class="row text-center">' +
+    '	        <div class="col-md-8 col-md-offset-2 text-center">' +
+    '	        	GET READY TO EXPERIENCE YOUR OWN PERSONALIZED LOOK INTO THE FUTURE OF RETAIL' +
+    '	        </div>' +
+    '        </div>' +
+    '   </div>' +		
+    '</section>'
+);
